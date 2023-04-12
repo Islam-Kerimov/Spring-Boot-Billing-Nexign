0a8681e3da83335a078f5e28f244023e1ecaa152
Islam Kerimov <kerimovikh@mail.ru>
2023-04-12 18:16:23 +0400

    init commit

---
+++ src/main/java/ru/nexign/spring/boot/billing/SpringBootBillingNexignApplication.java
@@ -0,0 +1,13 @@
+package ru.nexign.spring.boot.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBillingNexignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBillingNexignApplication.class, args);
+    }
+
+}
